30994e93a792ee34d7493e1eefa63e7213bf5a27
Roberta Giambona <rgiambon@cbr12s1.42barcelona.com>
2025-02-12 12:24:12 +0100

    feat: sphere struct

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,21 +6,32 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:28:41 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/12 11:51:18 by tatahere         ###   ########.fr       */
+/*   Updated: 2025/02/12 12:10:58 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef	MINIRT_H
+#define MINIRT_H
+
+#include "vector_math.h"
 
 typedef	struct s_camera
 {
-
+	t_vec3	origin;
+	t_vec3	direction;
+	t_vec3	fov;
 }	t_camera;
 
 typedef struct s_sphere
 {
+	t_vec3	*center;
+	double	diameter;
+	//colors
 }	t_sphere;
 	
 typedef	struct s_cene
 {
-	t_camera	camera;
+	t_camera	*camera;
 	t_list		*spheres;
 }
+
+#endif
