6bf27d9573854f5be892c6c30b12fc412960a4a3
Roberta Giambona <rgiambon@cbr12s1.42barcelona.com>
2025-02-12 13:16:41 +0100

    feat: light stuct

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,7 +6,7 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:28:41 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/12 12:52:09 by rgiambon         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:16:26 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef	MINIRT_H
@@ -20,6 +20,13 @@ typedef struct s_colors
 	int	g;
 	int	b;
 }	t_colors;
+
+typedef struct s_light
+{
+	vec3		pov;
+	double		brightness;
+	t_colors	colors;
+}	t_light;
 
 typedef	struct s_camera
 {
