2c2074b9f0d449da8ab408b1a5ed69f84e496af4
Roberta Giambona <rgiambon@cbr12s1.42barcelona.com>
2025-02-12 13:02:54 +0100

    wip: sphere parsing

--- inc/minirt.h
+++ inc/minirt.h
@@ -6,13 +6,20 @@
 /*   By: tatahere <tatahere@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 11:28:41 by tatahere          #+#    #+#             */
-/*   Updated: 2025/02/12 12:10:58 by rgiambon         ###   ########.fr       */
+/*   Updated: 2025/02/12 12:52:09 by rgiambon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef	MINIRT_H
 #define MINIRT_H
 
 #include "vector_math.h"
+
+typedef struct s_colors
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_colors;
 
 typedef	struct s_camera
 {
@@ -23,9 +30,9 @@ typedef	struct s_camera
 
 typedef struct s_sphere
 {
-	t_vec3	*center;
-	double	diameter;
-	//colors
+	t_vec3		*center;
+	double		diameter;
+	t_colors	*colors;
 }	t_sphere;
 	
 typedef	struct s_cene
